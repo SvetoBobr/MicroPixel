e00ac86495bd8237dcd1b1b16c1053187b87ed53
Mihal <bobropochta@gmail.com>
2019-09-07 19:48:54 +0300

    4 LED project started

---
+++ G3/poi/micropixel_4led_modes.c
@@ -0,0 +1,356 @@
+
+
+void make_serie(unsigned char sss){
+	switch (sss){
+			case 1:
+				mode_1();
+				break;;
+			case 2:
+				mode_2();
+				break;;
+			case 3:
+				mode_3();
+				break;;
+		}
+		
+		switch (sss){
+			case 4:
+				mode_4();
+				break;;
+			case 5:
+				mode_5();
+				break;;
+			case 6:
+				mode_6();
+				break;;
+		}
+		switch (sss){
+			case 7:
+				mode_7();
+				break;;		
+			case 8:
+				mode_8();
+				break;;	
+			case 9:
+				mode_9();
+				break;;							
+			case 10:
+				mode_10();
+				break;;
+		}
+		switch (sss){
+			case 13:
+				mode_4_18();
+				//svetobobr();
+				break;;		
+			case 11:
+				heart();
+				break;;	
+			case 12:
+				binary();
+				break;;							
+			case 14:
+				mode_8_18();
+				//demo();
+				break;;
+		}
+		switch (sss){
+			case 15: 
+				svetobobr();
+				break;;
+			case 16:
+				demo();
+				break;;
+			case 17:
+				demo_long();
+				break;
+		}
+	
+}
+
+
+void formColorPack( unsigned char n, struct RGB* color, rgb_pointer pack[], unsigned char mask ){
+	unsigned char i;
+	for (i=0; i<n; i++){
+		pack[i]= pal[0];
+		if ( (i<8) && ( (mask&(1<<i))==(1<<i) ) ) pack[i]=color;
+	}
+}
+
+
+void formColorPack_scheme( unsigned char n, rgb_pointer color[], rgb_pointer pack[], unsigned char mask  ){
+	unsigned char i;
+	for (i=0; i<n; i++){
+		pack[i]= pal[0];
+		if ( (i<8) && ( (mask&(1<<i))==(1<<i) ) ) pack[i]=color[i];
+	}	
+}
+
+
+void const_light_legacy(unsigned char mode, unsigned char t, unsigned char ledmask){
+	flush_mask();
+	if ( (ledmask & 1) == 1) mask[1]=1;
+	if ( (ledmask & 2) == 2) mask[2]=1;
+	if ( (ledmask & 4) == 4) mask[3]=1;
+	if ( (ledmask & 8) == 8) mask[4]=1;
+	
+	if ( (ledmask & 16) == 16) mask[5]=1;
+	if ( (ledmask & 32) == 32) mask[6]=1;
+	if ( (ledmask & 64) == 64) mask[7]=1;
+	if ( (ledmask & 128) == 128) mask[8]=1;
+	const_light(mode, t);
+}
+
+void mode_1(){	// constd
+	const_light_legacy(mode, 10, 255 );
+}
+
+void mode_2(){	// strob
+	const_light_legacy(mode, 6, 255 );
+	const_light_legacy(0, 6, 255 );
+}
+
+void mode_3(){	// gears
+	const_light_legacy(mode, 6, 1+2+4+8 );
+	const_light_legacy(mode, 6, 255-(1+2+4+8) );
+}
+
+void mode_4(){	// flower
+	const_light_legacy(mode, 3, LED1 );
+	const_light_legacy(mode, 3, LED2 );
+	const_light_legacy(mode, 3, LED3 );
+	const_light_legacy(mode, 3, LED4 );
+	const_light_legacy(mode, 3, LED5 );
+	const_light_legacy(mode, 3, LED6 );
+	const_light_legacy(mode, 3, LED7 );
+	const_light_legacy(mode, 2, 255 );
+}
+
+void mode_4_18(){	// not really flower from mpx 18
+	unsigned static a;
+	unsigned char i;
+	for ( i=1; i<=LED_NUM; i++ ) {
+		if ( i==1 || i==LED_NUM-a || i==1 || i==LED_NUM) mask[i]=1; else mask[i]=0;
+	}
+	const_light(mode, 1 );
+	if (++a>LED_NUM) a=0;
+}
+
+void mode_5(){	// romb 1
+	const_light_legacy(0, 3, 0 );
+	const_light_legacy(mode, 3, LED4+LED5 + 0 );
+	const_light_legacy(mode, 3, LED3+LED6 + 0 );
+	const_light_legacy(mode, 3, LED3+LED6 + LED2+LED7 );
+	const_light_legacy(mode, 6, LED1+LED8 + LED4+LED5 );
+	const_light_legacy(mode, 3, LED3+LED6 + LED2+LED7 );
+	const_light_legacy(mode, 3, LED3+LED6 + 0 );
+	const_light_legacy(mode, 3, LED4+LED5 + 0 );
+}
+
+void mode_6(){	// chain
+	const_light_legacy(mode, 11, LED3 + LED4 + LED5 + LED6 );
+	const_light_legacy(mode, 3, 0 + LED4 + LED5 + 0 );
+	const_light_legacy(mode, 3, LED2 + LED3 + LED4 + LED5 + LED6 + LED7 );
+	const_light_legacy(mode, 3, LED1 + LED2 + LED3 + LED6 + LED7 + LED8 );
+	const_light_legacy(mode, 9, LED1 + LED2 + LED7 + LED8 );
+	const_light_legacy(mode, 3, LED1 + LED2 + LED3 + LED6 + LED7 + LED8 );
+	const_light_legacy(mode, 3, LED2 + LED3 + LED4 + LED5 + LED6 + LED7 );
+	const_light_legacy(mode, 3, 0 + LED4 + LED5 + 0 );
+}
+
+void mode_7(){	//flash (molnia)
+	const_light_legacy(mode, 3, LED2 );
+	const_light_legacy(mode, 3, LED1 + 0 + 0 + 0 + 0 + 0 );
+	const_light_legacy(mode, 3, LED2 + LED3 + LED8 );
+	const_light_legacy(mode, 3, 0 + 0 + LED4 + 0 + 0 + LED7 );
+	const_light_legacy(mode, 9, LED2 + LED3 + LED4 + LED5 + LED6 + LED7 );
+	const_light_legacy(mode, 3, 0 + LED3 + 0 + LED5 + 0 + 0 );
+	const_light_legacy(mode, 3, 0 + LED3 + 0 + LED5 + LED6 + 0 );
+	const_light_legacy(mode, 3, LED2 + LED7 );
+	const_light_legacy(mode, 6, LED1 + LED6  );
+	const_light_legacy(mode, 3, LED2 + LED6 );
+	const_light_legacy(mode, 3, LED2 + LED5 + 0 + LED7 );
+	const_light_legacy(mode, 3, LED3 + LED4 + LED7 );
+	const_light_legacy(mode, 3, LED3 + LED8 );
+	const_light_legacy(mode, 3, LED4 + LED7 );
+	const_light_legacy(mode, 3, LED3 + LED4 + LED8 );
+	const_light_legacy(mode, 3, LED2 + LED8 );
+	const_light_legacy(mode, 3, LED2  );
+}
+
+void mode_8(){	//fire-flower
+	const_light_legacy(mode, 3, 0 );
+	const_light_legacy(mode, 3, LED6 + LED7 );
+	const_light_legacy(mode, 3, LED6 + LED7 + LED3 + LED4 + LED5);
+	const_light_legacy(mode, 3, 0 + LED2 + LED3 + LED4 + LED5);
+	const_light_legacy(mode, 3, LED1 + LED2 + LED3 + LED4 + LED5 + LED6 + 0 );
+	const_light_legacy(mode, 3, LED1 + LED2 + LED3 + LED4 + LED5 + LED6 + LED7 + LED8);
+	const_light_legacy(mode, 3, LED1 + LED2 + LED3 + LED4 + LED5 + LED6 + 0 );
+	const_light_legacy(mode, 3, 0 + LED2 + LED3 + LED4 + LED5);
+	const_light_legacy(mode, 3, LED6 + LED7 + LED3 + LED4 + LED5);
+	const_light_legacy(mode, 3, LED6 + LED7 );
+}
+
+void mode_8_18(){ // cool 
+	unsigned static a;
+	unsigned char i;
+	for ( i=1; i<=LED_NUM; i++ ) {
+		if ( (((i+a) & 2)>0) || (i==1) || (i==LED_NUM) ) mask[i]=1; else mask[i]=0;
+	}
+	const_light(mode, 1 );
+	a++;
+	if (a>LED_NUM) a=0;
+}
+
+void mode_9(){	// 
+	const_light_legacy(mode, 6, 0 );
+	const_light_legacy(mode, 6, LED6 + LED7 + LED8 );
+	const_light_legacy(mode, 3, LED6 + LED7 + LED8 + LED3);
+	const_light_legacy(mode, 6, LED4 + LED5 );
+	const_light_legacy(mode, 3, LED1 + LED2 + LED3 + LED6);
+	const_light_legacy(mode, 6, LED1 + LED2 + LED3 );
+}
+
+void mode_10(){	//	? !
+	const_light_legacy(mode, 3, 0 );
+	const_light_legacy(mode, 3, LED6 + LED7 );
+	const_light_legacy(mode, 3,  LED8 );
+	const_light_legacy(mode, 3, LED1 + LED3 + LED4 + 0 + LED8 );
+	const_light_legacy(mode, 3, LED5 + LED8 );
+	const_light_legacy(mode, 3, LED6 + LED7 );
+	const_light_legacy(mode, 3, 0 );
+	const_light_legacy(mode, 3, 255 - 2 );
+	
+}
+
+		
+void heart(){
+	const_light_legacy(mode, 3, 0 );
+	const_light_legacy(mode, 3, LED5 + LED6 + LED7);
+	const_light_legacy(mode, 3, LED5 + LED6 + LED7 + LED4 + LED8);
+	const_light_legacy(mode, 3, LED5 + LED6 + LED7 + LED4 + LED8 + LED3);
+	const_light_legacy(mode, 3, 126 );
+	const_light_legacy(mode, 3, 1+2+4+8 );
+	const_light_legacy(mode, 3, 126 );
+	const_light_legacy(mode, 3, LED5 + LED6 + LED7 + LED4 + LED8 + LED3);
+	const_light_legacy(mode, 3, LED5 + LED6 + LED7 + LED4 + LED8);
+	const_light_legacy(mode, 3, LED5 + LED6 + LED7);
+	
+}
+
+void binary(){
+	static unsigned char i=0;
+	static char a=1;
+	const_light_legacy(mode, 3, i+=a);
+	if (i==255) a=-1;;
+	if (i==0) a=1;
+}
+
+#define SP_L	2
+#define	SB_L	1
+
+void svetobobr(){
+	const_light_legacy(mode, 1, LED5+LED6 );	//S
+	const_light_legacy(mode, 2, LED1+LED4+LED7 );
+	const_light_legacy(mode, 1, LED2+LED3 );	
+	
+	const_light_legacy(mode, 2, 0+0 + 0+0 );	// pause
+	
+	const_light_legacy(mode, 1, LED5+LED6+LED7 );	//V
+	const_light_legacy(mode, 1, LED3+LED4 );
+	const_light_legacy(mode, 1, LED1+LED2 );
+	const_light_legacy(mode, 1, LED3+LED4 );
+	const_light_legacy(mode, 1, LED5+LED6+LED7 );
+	
+	const_light_legacy(mode, 2, 0+0 + 0+0 ); // pause
+	
+	const_light_legacy(mode, 1, 127 );	//e
+	const_light_legacy(mode, 3, LED1+LED7 + 0+LED4 );
+	
+	const_light_legacy(mode, 2, 0+0 + 0+0 );	// pause
+	
+	const_light_legacy(mode, 2, 0+0 + LED7+0 );			//t
+	const_light_legacy(mode, 1, 127 );
+	const_light_legacy(mode, 2, 0+0 + LED7+0 );
+	
+	const_light_legacy(mode, 2, 0+0 + 0+0 ); // pause
+	
+	const_light_legacy(mode, 1, 0+LED2 + LED3+ LED4+LED5+LED6 );	//O
+	const_light_legacy(mode, 2, LED1+0 + 0+LED7 );
+	const_light_legacy(mode, 1, 0+LED2 + LED3+ LED4+LED5+LED6 );
+	
+	const_light_legacy(mode, 2, 0+0 + 0+0 ); // pause
+	
+	const_light_legacy(mode, 1, 127 );	//b
+	const_light_legacy(mode, 2, LED1+LED7 + 0+LED4 );
+	const_light_legacy(mode, 1, LED2+LED3 + LED5+LED6 );
+	
+	const_light_legacy(mode, 2, 0+0 + 0+0 ); // pause
+	
+	const_light_legacy(mode, 1, 0+LED2 + LED3+ LED4+LED5+LED6 );	//O
+	const_light_legacy(mode, 2, LED1+0 + 0+LED7 );
+	const_light_legacy(mode, 1, 0+LED2 + LED3+ LED4+LED5+LED6 );
+	
+	const_light_legacy(mode, 2, 0+0 + 0+0 );	// pause
+					
+	const_light_legacy(mode, 1, 127 );	//b
+	const_light_legacy(mode, 2, LED1+LED7 + 0+LED4 );
+	const_light_legacy(mode, 1, LED2+LED3 + LED5+LED6 );
+	
+	const_light_legacy(mode, 2, 0+0 + 0+0 );	// pause
+	
+	const_light_legacy(mode, 1, 127 );	//r
+	const_light_legacy(mode, 1, LED4+LED7 );
+	const_light_legacy(mode, 1, LED3+LED7 );
+	const_light_legacy(mode, 1, LED1+LED2+LED5+LED6 );
+	
+	const_light_legacy(mode, 5, 0+0 + 0+0 );	// big pause
+}
+
+
+
+void demo(){
+	static unsigned char ds=1;
+	static unsigned char dm=1;
+	unsigned char rm=1;
+
+	rm=mode;
+	mode=dm;
+	make_serie(ds);
+	mode=rm;
+	
+	if (++dm>DEMO_M_NUM){
+		dm=1;
+		
+		if (++ds>DEMO_S_NUM){
+			ds=1;
+		}
+	}
+}
+
+void demo_long(){
+	static unsigned char ds=1;
+	static unsigned char dm=1;
+	static unsigned char counter=0;
+	unsigned char rm=1;
+
+	rm=mode;
+	mode=dm;
+	make_serie(ds);
+	mode=rm;
+	
+	if (++counter==255){
+		counter=0;
+		if (++dm>DEMO_M_NUM){
+			dm=1;
+			
+			if (++ds>DEMO_S_NUM){
+				ds=1;
+			}
+		}
+	}
+}
+
+
+
+
---
+++ G3/poi/poi-4LED-v2-1.c
@@ -0,0 +1,473 @@
+// t85	attiny85	38400 prog
+// board	MMCU		programm_after_make
+
+// 8LED bike backlight
+
+
+// ***************** HARDWARE SECTION ****************************************************
+#define CPU_TYPE	T45
+#define F_CPU 8000000
+#define	PROP_TYPE	MICROPIXEL	// more info in "opt-api.c"
+
+#define LED_PORT	PORTB
+#define BOARD GDCV	//use GDCV for actual 30-60 l/m led strip 
+#include "../../boards.h"
+
+#define WLED_PIN	3
+// ***************** BASIC GLOW SETTINGS & MODES *****************************************
+
+#define FRAME_DELAY 1	// there are some problems wth this constant
+#define MAX_LEVEL	255
+#define HALF_LEVEL	60
+#define PACK_SIZE	8
+
+/*
+#define FRAME_DELAY NO_DELAY	// as fast as MCU can
+#define MAX_LEVEL	60
+#define HALF_LEVEL	20			// brightness in economic mode
+
+#define PACK_SIZE	8
+#define LED_NUM	PACK_SIZE
+*/
+
+// patterns and colors	
+// totally changed between G2 and G3 - be careful
+#define MODE_GATE	6
+
+#if MODE_GATE>MODE_NUM
+	#undef MODE_GATE
+	#define MODE_GATE	MODE_NUM
+#endif
+
+//modes
+
+#include "./micropixel_8led_modes.h"
+
+
+
+unsigned char mode;
+unsigned char serie;//, ss;
+unsigned char power;
+
+
+// ***************** ADVANCED BEHAVIOR & MODULES *****************************************
+// uncoment to enable fav 
+#define USE_FAV
+#define FAV_MAX	15
+// comment not to use
+#define SOFTWARE_SLEEP
+#define ECONOM_ENABLED
+
+
+// #define RESET_MODE_ON_SERIE	// uncomment to start each serie fron 1st mode
+
+#define INC_DELAY	14
+#define S_DELAY		100
+#define ECONOM_DELAY	700
+#define HALT_DELAY  	1500
+#define W_FAV_DELAY		3000
+#define T_FAV_DELAY		10000
+#define CLEAR_DELAY 	18000
+
+#define WAKE_THRESHOLD	20
+
+#ifdef ECONOM_ENABLED
+	unsigned char econom=0;
+//	unsigned char level;
+#endif
+
+void set_level(unsigned char );
+
+//#include "../fav-api.h"
+
+#include "../apa102-driver-2.c"
+#include "../palette_7__low.c"
+#include "../memory-api.c"
+#include "../power-api.c"
+#include "../button-api.c"
+#include "../opt-api.c"
+
+
+// =========================================================================================================
+// =================== MAIN SECTION ========================================================================
+// =========================================================================================================
+
+// >>>>>>>>>>> service functions & vars >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+unsigned char r[PACK_SIZE];
+unsigned char g[PACK_SIZE];
+unsigned char b[PACK_SIZE];
+unsigned char mask[PACK_SIZE+1];
+
+	rgb_pointer *pal;
+	
+unsigned char bbb=0;
+
+void formColorPack( unsigned char n, struct RGB* color, rgb_pointer pack[], unsigned char mask  );
+void formColorPack_scheme( unsigned char n, rgb_pointer color[], rgb_pointer pack[], unsigned char mask  );
+
+void fill_mask();
+void flush_mask();
+
+inline unsigned char const_light( unsigned char sch,  unsigned char delay );
+
+// >>>>>>>>>>> modes  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+void  make_serie(unsigned char sss);
+
+#include "./micropixel_8led_modes.c"
+
+// >>>>>>>>>> main part >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+int main(){
+	read_data();
+	check_all();
+	
+	power=ACTIVE;
+	stat=0;
+	//ss=0;
+	
+	init_io();
+	
+	econom=0;
+
+	cli();
+	
+	//serie=9;
+/*
+	mr=fr;
+	mg=fg;
+	mb=fb;		
+	*/
+	unsigned char res_mode=0;
+	for(;;){ 
+		bbb=0;
+		cli();
+		
+		if ( power==IDLE ){
+				const_light(0, 1 );	// fade strip
+			DDRB=0;
+			PORTB=(1<<MODE_BUTTON);
+			//go_sleep();
+			set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+			sleep_enable();
+			_interrupts_on();
+			sleep_cpu();
+			sleep_disable();
+		} 	else if (stat==0) {
+			if (fav_on==0){
+				make_serie(serie);
+			} else {
+				if (counter>0){
+					res_mode=mode;
+					mode=modes[pointer];
+					make_serie(series[pointer]);
+					mode=res_mode;
+				} else {
+					process_signal(SIGNAL_UNDEFINED_STATE);
+				}
+			}
+		}	else {
+			process_signal(stat);
+		} 
+
+	}
+	
+	return 0;
+}
+
+void flush_mask(){
+	unsigned char i;
+	for ( i=0; i<=LED_NUM; i++ )	mask[i]=0;
+}
+
+void fill_mask(){
+	unsigned char i;
+	for ( i=0; i<=(LED_NUM); i++ )	mask[i]=1;
+}
+
+static unsigned char rgb[3];
+static unsigned char rgb2[3];
+static unsigned char rgb3[3];
+static unsigned char rgb4[3];
+
+	
+void set_level(unsigned char l){
+	//level=l;
+	rgb[0]=rgb2[0]=rgb3[0]=rgb4[2]=MAX_LEVEL;
+	rgb[1]=rgb2[1]=rgb3[1]=rgb[2]=rgb2[2]=rgb3[2]=rgb4[0]=rgb4[1]=0;
+
+}
+
+void l_shift(unsigned char arr[], unsigned char l){
+	unsigned char i;
+	unsigned char tmp=arr[0];
+	for (i=1; i<l; i++){
+		arr[i-1]=arr[i];
+	}
+	arr[l-1]=tmp;
+}
+
+void r_shift(unsigned char arr[], unsigned char l){
+	unsigned char i;
+	unsigned char tmp=arr[l-1];
+	for (i=l-1; i>0; i--){
+		arr[i]=arr[i-1];
+	}
+	arr[0]=tmp;
+}
+
+//unsigned char wave_r[PACK_SIZE]={255,220,170,83,0,0,0,0,0,0,83,170,220,255,255,255,255,255};
+//unsigned char wave_g[PACK_SIZE]={255,255,255,255,220,170,83,0,0,0,0,0,0,83,170,220,255,255};
+//unsigned char wave_b[PACK_SIZE]={0,0,0,0,0,0,83,170,220,255,255,255,255,255,255,220,170,83};
+
+unsigned char wave_1[PACK_SIZE]={255,195,140,95,55,25,5,0};
+unsigned char wave_2[PACK_SIZE]={255,195,140,95,55,25,5,0};
+unsigned char wave_3[PACK_SIZE]={50,150,255,150,50,0,0,0};
+//unsigned char wave_3[PACK_SIZE]={255,226,198,169,141,113,84,56,28,28,56,84,113,141,169,198,226,255};
+//unsigned char wave_2[PACK_SIZE]={0,0,15,30,60,105,150,200,255,255,200,150,105,60,30,15,0,0};
+unsigned char  const_light( unsigned char sch,  unsigned char delay){
+	static unsigned char rgbs=0;
+	static unsigned char tmpsch=1;
+	static unsigned char counter=0;
+	//static unsigned char b1=0;
+	unsigned char i, j;
+			 
+	if (bbb==1) return 1;
+	
+	for (i=0; i<delay; i++){
+		if (sch<8){
+			for (j=0; j<LED_NUM; j++){
+				r[j]=mr[sch];
+				g[j]=mg[sch];
+				b[j]=mb[sch];
+			}
+		} else {
+			switch (sch){
+				case 8:
+					for (j=0; j<LED_NUM; j++){
+						if ( (j<2) || (j>(LED_NUM-3)) ) {
+							r[j]=MAX_LEVEL; g[j]=MAX_LEVEL; b[j]=0;
+						} else {
+							r[j]=0; g[j]=MAX_LEVEL; b[j]=0;
+						}
+					}
+					break;
+				case 9:
+					for (j=0; j<LED_NUM; j++){
+						if ( (j<2) || (j>(LED_NUM-3)) ) {
+							r[j]=MAX_LEVEL; g[j]=0; b[j]=0;
+						} else {
+							r[j]=0; g[j]=0; b[j]=MAX_LEVEL;
+						}
+					}
+					break;
+				case 10:	// russian flag
+					r[0]=r[1]=g[0]=g[1]=b[0]=b[1]=150;
+					r[3]=r[4]=r[5]=r[2]=g[3]=g[4]=g[5]=g[2]=0;
+					r[6]=r[7]=b[2]=b[3]=b[4]=b[5]=255;
+					
+					break;
+				case 11:	// green-red
+					r[0]=r[1]=r[2]=r[3]=g[4]=g[5]=g[6]=g[7]=255;
+					g[0]=g[1]=g[2]=g[3]=r[4]=r[5]=r[6]=r[7]=0;
+					b[0]=b[1]=b[2]=b[3]=b[4]=b[5]=b[6]=b[7]=0;
+					break;
+				case 12:	// blue-red
+					r[0]=r[1]=r[2]=r[3]=b[4]=b[5]=b[6]=b[7]=255;
+					g[0]=g[1]=g[2]=g[3]=r[4]=r[5]=r[6]=r[7]=0;
+					b[0]=b[1]=b[2]=b[3]=g[4]=g[5]=g[6]=g[7]=0;
+					break;					
+					/*
+				case 12:	// blue-yellow
+					r[0]=r[1]=r[2]=r[3]=150;
+					g[4]=g[5]=g[6]=g[7]=0;
+					g[0]=g[1]=g[2]=g[3]=150;
+					r[4]=r[5]=r[6]=r[7]=0;
+					b[0]=b[1]=b[2]=b[3]=0;
+					b[4]=b[5]=b[6]=b[7]=255;
+					break;*/
+					
+			}
+			switch (sch){
+				case 13:	// b-r-g
+					for (j=0; j<LED_NUM; j++){
+						r[j]=g[j]=b[j]=0;
+						//r[j]=255; r[j+1]=0; r[j+2]=0;
+						//g[j]=0; g[j+1]=0; g[j+2]=255;
+						//b[j]=0; b[j+1]=255; b[j+2]=0;
+					}
+					r[0]=b[1]=g[2]=r[3]=b[4]=g[5]=r[6]=b[7]=255;
+					break;
+				case 14:	// GR rainbow
+					for (j=0; j<LED_NUM; j++){
+						r[j]=rgb2[0]; 
+						g[j]=rgb2[1]; 
+						b[j]=rgb2[2]; 
+					}
+					if (rgbs==0) {	// r>y>g
+						rgb2[1]++;
+						rgb2[0]--;
+						if (rgb2[1]==MAX_LEVEL) rgbs++;
+					} else if (rgbs==1) {	// g>aq>b
+						rgb2[1]--;
+						rgb2[0]++;
+						if (rgb2[0]==MAX_LEVEL) rgbs=0;
+					} 
+					break;
+				}
+				switch (sch){
+				case 15:	// RB rainbow
+					for (j=0; j<LED_NUM; j++){
+
+							r[j]=rgb3[0]; 
+							g[j]=rgb3[1]; 
+							b[j]=rgb3[2]; 
+						
+					}
+					if (rgbs==0) {	// r>y>g
+						rgb3[2]++;
+						rgb3[0]--;
+						if (rgb3[2]==MAX_LEVEL) rgbs++;
+					} else if (rgbs==1) {	// g>aq>b
+						rgb3[2]--;
+						rgb3[0]++;
+						if (rgb3[0]==MAX_LEVEL) rgbs=0;
+					} 
+					break;
+				case 16:	// GB rainbow
+					for (j=0; j<LED_NUM; j++){
+							r[j]=rgb4[0]; 
+							g[j]=rgb4[1]; 
+							b[j]=rgb4[2]; 
+					}
+					if (rgbs==0) {	// r>y>g
+						rgb4[1]++;
+						rgb4[2]--;
+						if (rgb4[1]==MAX_LEVEL) rgbs++;
+					} else if (rgbs==1) {	// g>aq>b
+						rgb4[1]--;
+						rgb4[2]++;
+						if (rgb4[2]==MAX_LEVEL) rgbs=0;
+					} 
+					break;
+				case 17:	// full rainbow
+					for (j=0; j<LED_NUM; j++){
+							r[j]=rgb[0]; 
+							g[j]=rgb[1]; 
+							b[j]=rgb[2]; 
+					}
+					
+					if (rgbs==0) {	// r>y>g
+						rgb[1]++;
+						rgb[0]--;
+						if (rgb[1]==MAX_LEVEL) rgbs++;
+					} else if (rgbs==1) {	// g>aq>b
+						rgb[2]++;
+						rgb[1]--;
+						if (rgb[2]==MAX_LEVEL) rgbs++;
+					} else if (rgbs==2) {	// b>v>r
+						rgb[2]--;
+						rgb[0]++;
+						if (rgb[2]==0) rgbs=0;
+					} else rgbs=0;
+					break;
+				case 18: 
+					for (j=0; j<LED_NUM; j++){
+						r[j]=mr[tmpsch];
+						g[j]=mg[tmpsch];
+						b[j]=mb[tmpsch];
+					}
+					if (++tmpsch>8) tmpsch=1;
+			}
+			switch (sch){
+				case 19:
+					if (++counter>=60){
+						counter=0;
+						l_shift(wave_1, LED_NUM);
+						r_shift(wave_2, LED_NUM);
+					}
+					for (j=0; j<LED_NUM; j++){
+						r[j]=wave_1[j];
+						g[j]=wave_2[j];
+						b[j]=0;
+					} 
+					break;
+				case 20:
+					if (++counter>=60){
+						counter=0;
+						l_shift(wave_1, LED_NUM);
+						r_shift(wave_2, LED_NUM);
+					}
+					for (j=0; j<LED_NUM; j++){
+						r[j]=wave_1[j];
+						g[j]=0;
+						b[j]=wave_2[j];
+					}
+					break;
+				case 21:
+					if (++counter>=60){
+						counter=0;
+						l_shift(wave_1, LED_NUM);
+						r_shift(wave_2, LED_NUM);
+					}
+					
+					for (j=0; j<LED_NUM; j++){
+						r[j]=0;
+						g[j]=wave_1[j];
+						b[j]=wave_2[j];
+					}
+					break;
+				case 22:
+					if (++counter>=60){
+						counter=0;
+						l_shift(wave_1, LED_NUM);
+						r_shift(wave_2, LED_NUM);
+					}
+					
+					for (j=0; j<LED_NUM; j++){
+						r[j]=wave_3[j];
+						g[j]=wave_1[j];
+						b[j]=wave_2[j];
+					}
+					break;
+			}
+		}		
+			
+		if ( check_button()==1 ) {
+			bbb=1;
+			return 1;	
+		}
+		
+		for (j=0; j<=LED_NUM; j++)
+			if (econom==1){
+				r[j]=r[j]>>2;
+				g[j]=g[j]>>2;
+				b[j]=b[j]>>2;
+			} else if (econom==2){
+				r[j]=r[j]>>4;
+				g[j]=g[j]>>4;
+				b[j]=b[j]>>4;	
+			} else if (econom==3){
+				r[j]=r[j]>>6;
+				g[j]=g[j]>>6;
+				b[j]=b[j]>>6;	
+			}
+		sendRawRGBpack(LED_NUM, r, g, b, mask);	
+	}
+	
+	return 0;
+}
+
+// >>>>>>>>>>>>>> modes section >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#if LED_NUM==8
+	#define M_DELAY	140
+	#define M_DELAY2	60
+	#define M_DELAY3	15
+	#define M_DELAY4	1
+#endif
+
+#if LED_NUM==15
+	#define M_DELAY	80
+	#define M_DELAY2	35
+	#define M_DELAY3	8
+	#warning 15 leds
+#endif
